4358398abe0a7699bfa23a716d2ac724683b30f8
Vishal Sharma <97242441+Vishalking758@users.noreply.github.com>
2023-05-30 06:45:58 -0700

    publish

---
+++ javascript.js
@@ -0,0 +1,99 @@
+function red(){
+    document.getElementById("main-1").style.display="flex";
+    document.getElementById("main").style.display="none";
+}
+
+function green(){
+    document.getElementById("main-2").style.display="flex";
+    document.getElementById("main").style.display="none";
+}
+
+function blue(){
+    document.getElementById("main-3").style.display="flex";
+    document.getElementById("main").style.display="none";
+}
+
+function brown(){
+    document.getElementById("main-4").style.display="flex";
+    document.getElementById("main").style.display="none";
+}
+
+function closebtn(){
+    document.getElementById("main-1").style.display="none";
+    document.getElementById("main-2").style.display="none";
+    document.getElementById("main-3").style.display="none";
+    document.getElementById("main-4").style.display="none";
+    document.getElementById("main").style.display="flex";
+}
+
+function red1(){
+
+    document.getElementById("view-section").style.backgroundColor="red";
+
+}
+
+function red2(){
+
+    document.getElementById("view-section").style.backgroundColor="#f35e5e";
+
+}
+
+function red3(){
+
+    document.getElementById("view-section").style.backgroundColor="#ab1c1c";
+
+}
+
+function red4(){
+
+    document.getElementById("view-section").style.backgroundColor="#ad4343";
+
+}
+
+function green1(){
+
+    document.getElementById("view-section-2").style.backgroundColor="green";
+
+}
+
+function green2(){
+
+    document.getElementById("view-section-2").style.backgroundColor="#06ff06";
+
+}
+
+function green3(){
+
+    document.getElementById("view-section-2").style.backgroundColor="#5aa85a";
+
+}
+
+function green4(){
+
+    document.getElementById("view-section-2").style.backgroundColor="#244224";
+
+}
+
+function blue1(){
+
+    document.getElementById("view-section-3").style.backgroundColor="blue";
+
+}
+
+function blue2(){
+
+    document.getElementById("view-section-3").style.backgroundColor="#8b8bfd";
+
+}
+
+function blue3(){
+
+    document.getElementById("view-section-3").style.backgroundColor="#323286";
+
+}
+
+function blue4(){
+
+    document.getElementById("view-section-3").style.backgroundColor="#5b5bff";
+
+}
